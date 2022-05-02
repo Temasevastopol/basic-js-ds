157f3b6911ff382ff8606a77a8ce4ec25f17adf9
Tema_sevastopol <temasebastopol@yandex.ru>
2022-05-02 23:11:03 +0300

    feat: add solution stack

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+    constructor (){
+      this.items = []
+    }
+  push(element) {
+    this.items.push(element)
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    return this.items.pop()
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.items[this.items.length - 1]
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
