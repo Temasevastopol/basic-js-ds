f016dfb2eba6a764bb4aec62ead4d80e1107ba1e
Tema_sevastopol <temasebastopol@yandex.ru>
2022-10-17 23:25:41 +0300

    feat: remove solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,8 +22,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k ) {
+  for(let i = 0; i < l.length; i++){
+    if(l[i] == k){      
+      l.splice(i, 1)
+      i--;
+    }
+  }
+  return l
+ // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
